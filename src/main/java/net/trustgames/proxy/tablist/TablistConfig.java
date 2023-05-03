f426f23a1db0b31dfc27a40787b2ac1c89897375
ThomasWega <tomous.wega@gmail.com>
2023-05-03 13:04:21 +0200

    Fixed TablistConfig values (converted to MiniMessage format)
    Took 6 minutes

--- src/main/java/net/trustgames/proxy/tablist/TablistConfig.java
+++ src/main/java/net/trustgames/proxy/tablist/TablistConfig.java
@@ -1,12 +1,14 @@
 package net.trustgames.proxy.tablist;
 
+import lombok.Getter;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 
 public enum TablistConfig {
-    TABLIST_HEADER("&#ffa600&lTRUSTGAMES\n&#363c3dMiniGames Playground\n"),
-    TABLIST_FOOTER("\n&astore.trustgames.net");
+    TABLIST_HEADER("<color:#ffa600><b>TRUSTGAMES</b><newline><color:#363c3d>MiniGames Playground<newline>"),
+    TABLIST_FOOTER("<newline><green>store.trustgames.net");
 
+    @Getter
     private final String value;
 
     TablistConfig(String value) {
